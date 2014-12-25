abd7f399f9ed1ebc482684934da8c85765d5b801
Karshan Sharma <karshan.sharma@gmail.com>
2014-12-24 17:20:07 -0800

    use hmacSha512 verbatim instead of DRBG

--- Main.hs
+++ Main.hs
@@ -3,31 +3,30 @@
 module Main where
 
 import Control.Applicative ((<$>))
-import Crypto.Random.DRBG (HmacDRBG, newGen, reseed, genBytes, GenError)
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS (pack, unpack, take, foldr, length, index, readFile, putStrLn)
+import qualified Data.ByteString.Lazy.Char8 as LBS (pack, unpack)
 import Data.Char (ord)
-import Data.Default (Default(..))
+import Data.Digest.Pure.SHA (bytestringDigest, hmacSha512)
+import Data.Monoid ((<>))
 import System.Environment (getArgs)
 
 data PasswordSettings = PasswordSettings { len :: Int, prefix :: ByteString, charset :: ByteString, url :: ByteString, username :: ByteString } 
     deriving (Show, Read)
 
-genPassword :: ByteString -> PasswordSettings -> Maybe ByteString -> Either GenError ByteString
-genPassword key settings question = 
-    newGen key >>=
-    reseed (url settings) >>=
-    reseed (username settings) >>=
-    \(drbg::HmacDRBG) -> maybe (return drbg) (\q -> reseed q drbg) question >>=
-    genBytes (len settings) >>=
-    \(bytes, _) -> return $ BS.take (len settings) $ baseX bytes (charset settings)
+genPassword :: ByteString -> PasswordSettings -> ByteString -> ByteString
+genPassword key settings additional = BS.take (len settings) $ baseX (charset settings) $ bytestringDigest' $ hmacSha512 (lbs key) (lbs d)
+    where
+        d = (url settings) <> (username settings) <> additional
+        bytestringDigest' = BS.pack . LBS.unpack . bytestringDigest
+        lbs = LBS.pack . BS.unpack
 
 -- convert bytes to integer assuming little endian ordering
 readLE :: ByteString -> Integer
 readLE = BS.foldr (\e s -> (s * 256) + (fromIntegral $ ord e)) 0
 
 baseX :: ByteString -> ByteString -> ByteString
-baseX bs alphabet = BS.pack $ reverse $ map (BS.index alphabet . fromIntegral . (`mod` x)) $ takeWhile (> 0) $ iterate (`div` x) $ readLE bs
+baseX alphabet bs = BS.pack $ reverse $ map (BS.index alphabet . fromIntegral . (`mod` x)) $ takeWhile (> 0) $ iterate (`div` x) $ readLE bs
     where
         x :: Integer
         x = fromIntegral $ BS.length alphabet
@@ -36,4 +35,4 @@ main = do
     (keyfile:pwfile:_) <- getArgs
     key <- BS.readFile keyfile
     pwsettings <- (read . BS.unpack) <$> BS.readFile pwfile
-    either print BS.putStrLn $ genPassword key pwsettings Nothing
+    BS.putStrLn $ genPassword key pwsettings ""
